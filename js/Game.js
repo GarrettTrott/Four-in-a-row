8b801fda08cef73425f33bd0d9f8d46bfb006fd6
Garrett Trott <garretttrott@gmail.com>
2020-01-02 23:59:47 -0800

    finished all class constuctors and methods

--- js/Game.js
+++ js/Game.js
@@ -1,1 +1,15 @@
-class Game {}
+class Game {
+  constructor() {
+    this.board = new Board();
+    this.players = this.createPlayers();
+    this.ready = false;
+  }
+
+  createPlayers() {
+    const players = [
+      new Player("Jerome", 1, "#e15258", true),
+      new Player("Sally", 2, "#e59a13")
+    ];
+    return players;
+  }
+}
